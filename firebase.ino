0a660534da3ff072ac73f0e7bf2521ec819a9074
Rhamzel05 <161028824+Rhamzel05@users.noreply.github.com>
2025-03-10 03:32:44 +0800

    firebase.ino

--- firebase.ino
+++ firebase.ino
@@ -1,85 +1,78 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <DHT.h>
 #include <time.h>
 
+#define DHTPIN 4
+#define DHTTYPE DHT11
+#define LDRPIN 34
+#define THRESHOLD 1000 
+
 const char* ntpServer = "time.google.com";
-const long gmtOffset_sec = 8 * 3600; // GMT+8 for Philippines
+const long gmtOffset_sec = 8 * 3600;
 const int daylightOffset_sec = 0;
 
 const char* ssid = "Manji";
 const char* password = "12345678";
 const char* firestoreUrl = "https://firestore.googleapis.com/v1/projects/fir-add02/databases/(default)/documents/sensorData?key=AIzaSyCJ-OoMjfBPuVYnKHQY0RNVnjvZH3etuQw";
 
+DHT dht(DHTPIN, DHTTYPE);
+int nightCount = 0;
+bool isNight = false;
+
 void setup() {
-  Serial.begin(115200);
-  
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi...");
-  int max_attempts = 20;
-  while (WiFi.status() != WL_CONNECTED && max_attempts > 0) {
-    Serial.print(".");
-    delay(1000);
-    max_attempts--;
-  }
-  if (WiFi.status() == WL_CONNECTED) {
+    Serial.begin(115200);
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to WiFi...");
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(1000);
+    }
     Serial.println("\nConnected to WiFi!");
     configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  } else {
-    Serial.println("\nFailed to connect to WiFi.");
-    return;
-  }
+    dht.begin();
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-    struct tm timeinfo;
-    int retry_count = 5;
-    while (!getLocalTime(&timeinfo) && retry_count > 0) {
-      Serial.println("Retrying time sync...");
-      delay(1000);
-      retry_count--;
+    if (WiFi.status() == WL_CONNECTED) {
+        struct tm timeinfo;
+        if (!getLocalTime(&timeinfo)) {
+            Serial.println("Failed to obtain time.");
+            return;
+        }
+        char timestamp[30];
+        strftime(timestamp, sizeof(timestamp), "%Y-%m-%dT%H:%M:%S", &timeinfo);
+
+        float temperature = dht.readTemperature();
+        float humidity = dht.readHumidity();
+        int ldrValue = analogRead(LDRPIN);
+        Serial.print("LDR Value: "); Serial.println(ldrValue);
+        
+        if (ldrValue < THRESHOLD) {
+            if (!isNight) {
+                nightCount++;
+                isNight = true;
+            }
+        } else {
+            isNight = false;
+        }
+
+        HTTPClient http;
+        http.begin(firestoreUrl);
+        http.addHeader("Content-Type", "application/json");
+
+        String jsonData = String("{\"fields\": {") +
+            "\"temperature\": {\"stringValue\": \"" + String(temperature) + "\"}," +
+            "\"humidity\": {\"stringValue\": \"" + String(humidity) + "\"}," +
+            "\"daylight\": {\"stringValue\": \"" + String(nightCount) + "\"}," +
+            "\"timestamp\": {\"stringValue\": \"" + String(timestamp) + "\"}" +
+            "}}";
+
+        int httpResponseCode = http.POST(jsonData);
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+
+        http.end();
     }
-    if (retry_count == 0) {
-      Serial.println("Failed to obtain time.");
-      return;
-    }
-
-    char timestamp[30];
-    strftime(timestamp, sizeof(timestamp), "%Y-%m-%dT%H:%M:%S", &timeinfo);
-
-    HTTPClient http;
-    http.begin(firestoreUrl);
-    http.addHeader("Content-Type", "application/json");
-
-    String jsonData = String("{\"fields\": {") +
-        "\"temperature\": {\"stringValue\": \"25.5\"}," +
-        "\"humidity\": {\"stringValue\": \"60\"}," +
-        "\"daylight\": {\"stringValue\": \"21\"}," +
-        "\"timestamp\": {\"stringValue\": \"" + String(timestamp) + "\"}" +
-        "}}";
-
-    int httpResponseCode = http.POST(jsonData);
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-
-    if (httpResponseCode > 0) {
-      String response = http.getString();
-      Serial.println("Response: " + response);
-    } else {
-      Serial.println("Error: " + http.errorToString(httpResponseCode));
-    }
-
-    http.end();
-  } else {
-    Serial.println("WiFi disconnected. Attempting to reconnect...");
-    WiFi.disconnect();
-    WiFi.reconnect();
-    int retries = 10;
-    while (WiFi.status() != WL_CONNECTED && retries > 0) {
-      delay(1000);
-      retries--;
-    }
-  }
-
-  delay(5000);
+    delay(5000);
 }
